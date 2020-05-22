75e5931ae7f5194be01c2ca3a0a0416b6afacf4f
Johan <johan@macair2020>
2020-05-23 01:59:43 +0200

    add uuid validation

--- src/schema.ts
+++ src/schema.ts
@@ -1,15 +1,21 @@
 import Joi from '@hapi/joi'
 import { validate } from './middleware';
-import * as T from './type';
+import { types as T } from '@nexys/utils'
 
 export const id:Joi.ObjectSchema<{id:T.Id}> = Joi.object({
   id: Joi.number().required()
 });
 
-export const validateId = validate<{id:T.Id}>(id);
+export const uuid:Joi.ObjectSchema<{uuid:T.Uuid}> = Joi.object({
+  id: Joi.string().guid().required()
+});
 
 export const name:Joi.ObjectSchema<{name:string}> = Joi.object({
   id: Joi.string().required()
 });
+
+export const validateId = validate<{id:T.Id}>(id);
+
+export const validateUuid = validate<{uuid:T.Uuid}>(uuid);
 
 export const validateName = validate<{name:string}>(name);
