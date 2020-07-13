e867ab8909e98ba1410f8b3ae0b08ebe53364772
Johan <johan@macair2020>
2020-07-13 15:35:15 +0200

    updated handleresponse

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -10,18 +10,39 @@ const authHelpers = { readToken };
 
 export { authHelpers, Auth };
 
-export const handleResponse = async (
-  r: () => Promise<any> | any,
+export const defaultMessage = "an error occurred, see logs for more details";
+export const defaultStatus = 500;
+
+export const handleError = (err: any, messageOverride?: string) => {
+  const status: number = err.status || defaultStatus;
+  const expose: boolean = err.expose || false;
+  const error = err.body || { message: err.message || defaultMessage };
+
+  // if the error is not exposed display in the logs
+  if (!expose) {
+    console.error(JSON.stringify(error, null, 2));
+  }
+
+  const message = messageOverride || defaultMessage;
+  const body = expose ? error : { message };
+
+  return { status, body };
+};
+
+export const handleResponse = async <A = any>(
+  r: () => Promise<A> | A,
   ctx: Koa.Context,
-  errorOverride?: string
+  messageOverride?: string
 ) => {
   try {
     ctx.body = await r();
   } catch (err) {
-    ctx.status = err.status || 500;
-    ctx.body = err.error || { message: err.message };
-    const e = errorOverride ? Error(errorOverride) : err;
-    ctx.app.emit("error", e, ctx);
+    const { status, body } = handleError(err, messageOverride);
+    ctx.status = status;
+    ctx.body = body;
+    //const e = errorOverride ? Error(errorOverride) : err;
+    //https://github.com/koajs/koa/issues/1162
+    //ctx.app.emit('error', e);
   }
 };
 
