2a0179b5bb09b756d1e05d30c271c8e7ffa855cb
Johan <johan@macair2020>
2020-08-13 00:57:36 +0200

    add github to soo

--- src/sso/index.ts
+++ src/sso/index.ts
@@ -5,4 +5,7 @@ import * as Utils from "./utils";
 import Google from "./google";
 import * as GoogleUtil from "./google";
 
-export { SSOAbstract, Routes, Utils, GoogleUtil, Google };
+import Github from "./google";
+import * as GithubUtil from "./google";
+
+export { SSOAbstract, Routes, Utils, GoogleUtil, Google, Github, GithubUtil };
