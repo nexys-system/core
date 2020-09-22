cc437968e13bd0dc77a782a06f4636babdf5ef7a
Johan <johan@macair2020>
2020-09-22 13:30:10 +0200

    added meta routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import Router from "koa-router";
 import Cms from "./cms";
 import I18n from "./i18n";
 import Notification from "./notification";
+import Meta from "./meta";
 import * as Type from "./type";
 
 export { Cms, I18n, Notification, Type };
@@ -12,6 +13,7 @@ export default (productService: Type.ProductService) => {
   router.use("/cms", Cms(productService.CMS));
   router.use("/i18n", I18n(productService.I18n));
   router.use("/notification", Notification(productService.Notification));
+  router.use("/meta", Meta());
 
   return router.routes();
 };
