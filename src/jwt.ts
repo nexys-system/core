eb157989fe7a67d858405dae705e37acf272844b
Johan <johan@macair2020>
2020-10-20 14:56:35 +0200

    added secret from env

--- src/jwt.ts
+++ src/jwt.ts
@@ -1,7 +1,17 @@
-import JWT from 'jsonwebtoken';
+import JWT from "jsonwebtoken";
 
-const secretKey = 'mysecretkey';
+const dummySecretKey = "mysecretkey";
 
-export const sign = <A extends Object>(profile: A):string => JWT.sign(profile, secretKey);
+const secretKey = process.env.SECRET || dummySecretKey;
 
-export const verify = <A extends Object>(token:string):({iat: number} & A) => JWT.verify(token, secretKey) as {iat: number} & A;
+if (dummySecretKey === secretKey) {
+  console.warn(
+    "secret key has not been set in env var, taking default - this should NEVER be the case on prod"
+  );
+}
+
+export const sign = <A extends Object>(profile: A): string =>
+  JWT.sign(profile, secretKey);
+
+export const verify = <A extends Object>(token: string): { iat: number } & A =>
+  JWT.verify(token, secretKey) as { iat: number } & A;
