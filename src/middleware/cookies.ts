10bb75e013f1cafbe1c8bb5b507168335e105c5c
John B <john@nexys.ch>
2020-11-06 17:41:05 +0100

    add cooke fiel?

---
+++ src/middleware/cookies.ts
@@ -0,0 +1,9 @@
+import Cookies from "cookies";
+
+const tokenKey = "ACCESS_TOKEN";
+
+export const getToken = (cookies: Cookies): string | undefined =>
+  cookies.get(tokenKey);
+
+export const setToken = (token: string, cookies: Cookies) =>
+  cookies.set(tokenKey, token, { httpOnly: true });
