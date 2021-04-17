af250e1a4c4ada5cf692e2c759bcc55e0eaedf72
Johan <johan@macair2020>
2021-04-17 08:25:52 +0200

    add jwt test

--- src/jwt.test.ts
+++ src/jwt.test.ts
@@ -5,6 +5,8 @@ test("encode", () => {
   const e = { a: "a", b: "a" };
   const s = j.sign(e);
   const o = j.verify(s);
+  const r = { ...e, iat: o.iat };
 
-  expect({ ...e, iat: o.iat }).toEqual(o);
+  expect(r).toEqual(o);
+  expect(j.read(s)).toEqual(o);
 });
