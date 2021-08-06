3cc7acc30a09199dbbe9825d46dcce4b06170fbe
Johan <johan@macair2020>
2021-08-07 00:53:51 +0200

    clean type

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,23 +1,3 @@
-import fs from "fs";
-
-export interface OptionSet {
-  id: number;
-  name: string;
-}
-
-// https://bitbucket.org/echo_rm/ts-is-present/src/master/
-// but does not seem to work
-export function isDefined<T>(t: T | undefined): t is T {
-  return t !== undefined;
-}
-
-export interface File {
-  value: Buffer | fs.ReadStream;
-  filename: string;
-  size: number;
-  contentType: string;
-}
-
 export interface ObjectWithId<IId> {
   id: IId;
 }
