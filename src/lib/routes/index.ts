c4b8f80cc1589d2e1968653bf8a51e6aad8e761e
John B <john@nexys.ch>
2021-09-23 14:43:44 +0200

    [ci] upgrade version

--- src/lib/routes/index.ts
+++ src/lib/routes/index.ts
@@ -1,4 +1,5 @@
 import Product from "./product";
+import GraphQL from "./graphql";
 import * as UserManagement from "./user-management";
 
-export { Product, UserManagement };
+export { Product, UserManagement, GraphQL };
