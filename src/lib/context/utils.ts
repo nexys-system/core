a85dfb2ff6c48b5a850636ceb57fe198d0690971
John B <johan@nexys.ch>
2021-10-30 12:36:32 +0200

    add utils to context

---
+++ src/lib/context/utils.ts
@@ -0,0 +1,10 @@
+import * as T from './type';
+
+export const printAppToken = (decodedAppToken: T.AppTokenDecoded) => {
+    return {
+      instance: [decodedAppToken.instance, decodedAppToken.instanceName],
+      product: [decodedAppToken.product, decodedAppToken.productName],
+      env: Env[decodedAppToken.env],
+      issuedAt: new Date(decodedAppToken.iat * 1000)
+    };
+  };
