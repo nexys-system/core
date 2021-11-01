d8f1e58888ed749d2b25758824baddbd102f3058
John B <john@nexys.ch>
2021-11-01 11:28:03 +0100

    close #13

--- src/lib/routes/product/meta.ts
+++ src/lib/routes/product/meta.ts
@@ -10,11 +10,13 @@ const argvs: string[] = process.argv.slice(2);
 const sha: string = argvs[0] || "git_undefined_app_arg";
 const version: string = argvs[1] || "git_version_undefined_app_arg";
 
+const date = new Date();
+
 export default () => {
   const router: Router = new Router();
 
   router.get("/version", async (ctx: Koa.Context) => {
-    ctx.body = { sha, version };
+    ctx.body = { sha, version, date };
   });
 
   return router.routes();
