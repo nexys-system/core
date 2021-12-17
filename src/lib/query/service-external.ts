eaeb4f165d37add8414eb2e4d3e4cd4f60091872
John B <john@nexys.ch>
2021-12-17 14:38:44 +0100

    fix issue with model get

--- src/lib/query/service-external.ts
+++ src/lib/query/service-external.ts
@@ -16,7 +16,11 @@ class QueryServiceExternal extends AbstractServiceWData {
   appToken: string;
   dataModel: TF.Entity[];
 
-  constructor(appToken: string, dataModel: TF.Entity[], host: string = hostDefault) {
+  constructor(
+    appToken: string,
+    dataModel: TF.Entity[],
+    host: string = hostDefault
+  ) {
     super();
 
     this.host = host;
@@ -24,11 +28,14 @@ class QueryServiceExternal extends AbstractServiceWData {
     this.dataModel = dataModel;
   }
 
-  request = async <A>(path: string, data: any): Promise<A> => {
+  request = async <A>(
+    path: string,
+    method: "GET" | "POST",
+    data?: any
+  ): Promise<A> => {
     const url = this.host + path;
 
-    const method = "POST";
-    const body = JSON.stringify(data);
+    const body: string | undefined = data ? JSON.stringify(data) : undefined;
     const headers = {
       "Content-Type": "application/json",
       Authorization: "Bearer " + this.appToken,
@@ -43,20 +50,24 @@ class QueryServiceExternal extends AbstractServiceWData {
     return r.json();
   };
 
-  modelSet = async ():Promise<{message: string}> => this.request("/model/set", this.dataModel);
-
-  modelGet = async ():Promise<TF.Entity[]> => this.request("/model/get");
-
   data = async (query: Query): Promise<QueryResponse> =>
-    this.request("/data", query);
+    this.request("/data", "POST", query);
 
   mutate = async (query: Mutate): Promise<MutateResponse> =>
-    this.request("/mutate", query);
+    this.request("/mutate", "POST", query);
 
   aggregate = async (query: TA.Query): Promise<TA.ResponseAggregate> => {
     console.warn("this is an experimental feature, do not push on prod");
-    return this.request("/aggregate", query);
+    return this.request("/aggregate", "POST", query);
   };
+
+  // meta
+  // get and set model
+  modelSet = async (): Promise<{ message: string }> =>
+    this.request("/model/set", "POST", this.dataModel);
+
+  modelGet = async (): Promise<TF.Entity[]> =>
+    this.request("/model/get", "GET");
 }
 
 export default QueryServiceExternal;
