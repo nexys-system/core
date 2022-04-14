0d720f7c68939efcf8b2924f1037b10e60c9361f
John B <johan@nexys.ch>
2022-04-14 08:52:53 +0200

    Update config.ts

--- src/lib/scripts/config.ts
+++ src/lib/scripts/config.ts
@@ -1,4 +1,4 @@
 export const outFolder = "src/common/generated";
-export const host = "https://deploy.nexys.io"; // "http://localhost:8080/api"; //
+export const host = "https://app2.nexys.io/api"; // "http://localhost:8080/api"; //
 export const pathEnvVar = "/external/env-var?q=";
 export const pathAssets = "/external/data-model?q=";
