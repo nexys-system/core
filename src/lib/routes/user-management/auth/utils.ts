f1585360ee51b1dfd74840fa1a1cc665dac1251d
John B <johan@nexys.ch>
2022-08-13 02:35:42 +0200

    Update utils.ts

--- src/lib/routes/user-management/auth/utils.ts
+++ src/lib/routes/user-management/auth/utils.ts
@@ -11,3 +11,20 @@ export const getIPandUserAgent = (
 
   return { ip, userAgent };
 };
+    
+/**
+ * can't take the "normal" because it will return the internal docker ip, need to take the real-ip, added in proxy pass
+ *  see https://github.com/nexys-system/display-ip/blob/master/src/app.ts#L9
+ */
+const formatIP = (headers: {
+  [k: string]: string | string[] | undefined;
+}): string => {
+  const headerKey = "x-real-ip";
+  const realIP: string | string[] | undefined = headers[headerKey];
+
+  if (typeof realIP === "string") {
+    return realIP;
+  }
+
+  return "undefined";
+};
